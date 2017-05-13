60c31c09b66c134b7f71037cb448d385b9f10f96
Simon Leigh <crumpetybumpety@gmail.com>
2017-05-13 14:43:47 +0900

    added apostraphe support

--- recipeAPI/urls.py
+++ recipeAPI/urls.py
@@ -4,11 +4,11 @@ from recipeAPI import views
 
 app_name = 'recipeAPI'
 urlpatterns = [
-    url(r'^recipes/(?P<pk>[A-Za-z_]+)/$', views.RecipeDetail.as_view(), name='recipe_detail'),
-    url(r'^items/(?P<pk>[A-Za-z_]+)/$', views.ItemDetail.as_view(), name='item_detail'),
+    url(r'^recipes/(?P<pk>[A-Za-z_\']+)/$', views.RecipeDetail.as_view(), name='recipe_detail'),
+    url(r'^items/(?P<pk>[A-Za-z_\']+)/$', views.ItemDetail.as_view(), name='item_detail'),
     url(r'^items/$', views.ItemList.as_view(), name='item_list'),
-    url(r'^items/search/(?P<search_term>[A-Za-z_]+)/$', views.ItemSearch.as_view(), name='item_search'),
-    url(r'^recipes/search/(?P<search_term>[A-Za-z_]+)/$', views.RecipeSearch.as_view(), name='recipe_search'),
-    url(r'^manager/item/(?P<pk>[A-Za-z_]+)/$', views.ItemCreate.as_view(), name='item_create'),
-    url(r'^manager/recipe/(?P<pk>[A-Za-z_]+)/$', views.RecipeCreate.as_view(), name='recipe_create'),
+    url(r'^items/search/(?P<search_term>[A-Za-z_\']+)/$', views.ItemSearch.as_view(), name='item_search'),
+    url(r'^recipes/search/(?P<search_term>[A-Za-z_\']+)/$', views.RecipeSearch.as_view(), name='recipe_search'),
+    url(r'^manager/item/(?P<pk>[A-Za-z_\']+)/$', views.ItemCreate.as_view(), name='item_create'),
+    url(r'^manager/recipe/(?P<pk>[A-Za-z_\']+)/$', views.RecipeCreate.as_view(), name='recipe_create'),
 ]
